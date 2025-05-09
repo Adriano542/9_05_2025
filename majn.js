56418a820aa47e7ec2b4b334f1195c82aa28277a
Adriano <122603292+Adriano542@users.noreply.github.com>
2025-05-09 08:35:32 +0200

    ff

--- majn.js
+++ majn.js
@@ -1,1 +1,4 @@
-
+document.getElementById("kolor").addEventListener("input", function () {
+      const wybranyKolor = this.value;
+      document.getElementById("formularzContainer").style.backgroundColor = wybranyKolor;
+    });
